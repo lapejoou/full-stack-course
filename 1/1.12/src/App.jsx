fbaf9b4098ac5dd1659f8a14aee236514654c9ee
Lotta Lappalainen <lotta.lappalainen@tuni.fi>
2024-08-15 17:48:41 +0300

    1.13

--- 1/1.12/src/App.jsx
+++ 1/1.12/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
   const anecdotes = [
@@ -8,21 +8,29 @@ const App = () => {
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
+  ];
   
-  const handleClick = () => setSelected(Math.floor(Math.random() * 9))
-  
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Uint8Array(anecdotes.length));
+
+  const handleVote = () => {
+    const newPoints = [...points];
+    newPoints[selected] += 1;
+    setPoints(newPoints);
+  };
+
+  const handleClick = () => setSelected(Math.floor(Math.random() * anecdotes.length));
 
   return (
     <div>
-      {anecdotes[selected]}
-      <button onClick = {handleClick}>next anecdote</button>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleClick}>next anecdote</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
