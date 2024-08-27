a37937a8660315576dc17d245eab2327efdc6d8c
Lotta Lappalainen <lotta.lappalainen@tuni.fi>
2024-08-27 10:29:19 +0300

    4.6

--- 4/blogilista/utils/list_helper.js
+++ 4/blogilista/utils/list_helper.js
@@ -1,7 +1,21 @@
 const dummy = (blogs) => {
     return 1;
   }
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+}
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return null;
+
+    return blogs.reduce((favBlog, blog) => {
+        return blog.likes > favBlog.likes ? blog : favBlog;
+    })
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
   }
