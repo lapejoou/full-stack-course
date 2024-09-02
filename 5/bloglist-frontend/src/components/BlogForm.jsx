121855044949af803451cea17d2580f8fba1a66c
Lotta Lappalainen <lotta.lappalainen@tuni.fi>
2024-09-02 18:53:57 +0300

    exercises till 5.12

---
+++ 5/bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const BlogForm = ({ addBlog }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const createBlog = (event) => {
+    event.preventDefault()
+    addBlog({
+      author: author,
+      title: title,
+      url: url
+    })
+
+    setAuthor('')
+    setTitle('')
+    setUrl('')
+  }
+
+  return (
+    <form onSubmit={createBlog}>
+      <h1>Create a new blog</h1>
+      <div>
+            title
+        <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+            author
+        <input
+          type="text"
+          value={author}
+          name="Author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+            url
+        <input
+          type="text"
+          value={url}
+          name="Url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  )
+}
+
+export default BlogForm
